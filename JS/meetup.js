f6c8de3f81c774f1a7e3d55aa019303421b06fa4
Chris Horton <chris.horton@sbcglobal.net>
2017-09-10 10:42:33 -0700

    meetup streaming finished

--- JS/meetup.js
+++ JS/meetup.js
@@ -1,3 +1,19 @@
-var rtm = new RTM("wss://open-data.api.satori.com", "2d2EA104068cfeEA9D39CB4F3FD1DABB");
+function subscribeToMeetUp() {
+    var rtm = new RTM("wss://open-data.api.satori.com", "2d2EA104068cfeEA9D39CB4F3FD1DABB");
 
-var channel = rtm.subscribe("your-channel", RTM.SubscriptionMode.SIMPLE);
+    var channel = rtm.subscribe('meetups', RTM.SubscriptionMode.SIMPLE, {
+        filter: 'SELECT * FROM meetup WHERE type = "rsvp"'
+    });
+
+    channel.on('enter-subscribed', function () {
+        console.log('Subscribed to: ' + channel.subscriptionId);
+    });
+
+    channel.on('rtm/subscription/data', function (pdu) {
+        pdu.body.messages.forEach(function (msg) {
+            console.log('Got message:', msg);
+        });
+    });
+
+    rtm.start()
+}
